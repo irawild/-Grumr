af21c5832d915a64661a3458263d1f5eafed9b46
Rafael Lucchese <rafael.lucchese@gmail.com>
2015-06-04 22:12:33 -0300

    Mailing address corrected.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'rafael.lucchese@gmail.com; '; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'rafael.lucchese@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Você recebeu uma nova mensagem de contato do atravéz do Grumr.\n\n"."Seguem os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
