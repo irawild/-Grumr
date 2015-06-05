0acda108c046fb3d021091460879e363341ffab0
Rafael Lucchese <rafaellucchese@EAB-MacBook-Pro.local>
2015-06-05 09:39:12 -0300

    Changes to mail to include header information. Get phpinfo, as well.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -19,8 +22,13 @@ $message = $_POST['message'];
 $to = 'rafael@grumr.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Você recebeu uma nova mensagem de contato do atravéz do Grumr.\n\n"."Seguem os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
-$headers = "From: sac@grumr.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html\r\n";
+$headers .= 'From: From: noreply@grumr.com.br' . "\r\n" .
+'Reply-To: ' . $email_address . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
